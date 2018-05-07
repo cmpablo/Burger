480c0adf456dd5fa4ef27cfe75c6841a357774da
Christine Pablo <cmpablo@cpablo.local>
2018-05-07 14:39:14 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "127.0.0.1",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if (err) {
